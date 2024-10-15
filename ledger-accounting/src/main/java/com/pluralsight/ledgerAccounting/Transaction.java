c2a840b495fedf100a7a50abe80a68f1e61274a9
Deibcr <deibcr@gmail.com>
2024-10-15 11:47:22 -0500

    Reader method

--- ledger-accounting/src/main/java/com/pluralsight/ledgerAccounting/Transaction.java
+++ ledger-accounting/src/main/java/com/pluralsight/ledgerAccounting/Transaction.java
@@ -1,6 +1,42 @@
 package com.pluralsight.ledgerAccounting;
 
 public class Transaction {
-    String date;
+    private String date;
+    private  String time;
+    private String description;
+    private String vendor;
+    private double amount;
 
+    public Transaction(String date, String time, String description, String vendor, double amount) {
+        this.date = date;
+        this.time = time;
+        this.description = description;
+        this.vendor = vendor;
+        this.amount = amount;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%-12s %-10s %-25s %-15s $%10.2f", date, time, description, vendor, amount);
+    }
 }
