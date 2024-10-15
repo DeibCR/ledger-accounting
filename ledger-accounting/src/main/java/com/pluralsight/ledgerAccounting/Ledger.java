6d66d36d14439809181a0a6db510b8385ce7c848
Deibcr <deibcr@gmail.com>
2024-10-15 14:00:56 -0500

    Home Screen, payment and deposit methods

--- ledger-accounting/src/main/java/com/pluralsight/ledgerAccounting/Ledger.java
+++ ledger-accounting/src/main/java/com/pluralsight/ledgerAccounting/Ledger.java
@@ -1,12 +1,11 @@
 package com.pluralsight.ledgerAccounting;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 
 public class Ledger {
      ArrayList<Transaction> transactions= new ArrayList<>(); // List that store transaction objects
@@ -32,10 +31,78 @@ public class Ledger {
           fileReader.close();
      }
 
+     //Method to add a new transaction (deposit) by prompting user
+     public void addDeposit(Scanner scanner){
+          System.out.println("Please enter the details for the new deposit");
+
+          System.out.println("Date (yyyy-MM-dd): ");
+          String date= scanner.nextLine();
+
+          System.out.println("Time (HH:mm:ss): ");
+          String time= scanner.nextLine();
+
+          System.out.println("Description: ");
+          String description = scanner.nextLine();
+
+          System.out.println("Vendor: ");
+          String vendor= scanner.nextLine();
+
+          System.out.println("amount (positive number): ");
+          double amount = scanner.nextDouble();
+          scanner.nextLine();
+
+          if (amount <= 0){
+               System.out.println(" A deposit amount must be positive");
+               return;
+          }
+
+          //create a new deposit and add to the list
+          Transaction deposit = new Transaction(date,time,description,vendor,amount);
+          transactions.add(deposit);
+          System.out.println("Deposit added successfully");
+
+     }
+
+     //Method to add a new transaction (payment) by prompting user
+     public void addPayment(Scanner scanner){
+          System.out.println("Please enter the details for the new payment");
+
+          System.out.println("Date (yyyy-MM-dd): ");
+          String date= scanner.nextLine();
+
+          System.out.println("Time (HH:mm:ss): ");
+          String time= scanner.nextLine();
+
+          System.out.println("Description: ");
+          String description = scanner.nextLine();
+
+          System.out.println("Vendor: ");
+          String vendor= scanner.nextLine();
+
+          System.out.println("amount (negative number): ");
+          double amount = scanner.nextDouble();
+          scanner.nextLine();
+
+          if (amount <= 0){
+               System.out.println(" A payment amount must be negative");
+               return;
+          }
+
+          //create a new deposit and add to the list
+          Transaction payment = new Transaction(date,time,description,vendor,amount);
+          transactions.add(payment);
+          System.out.println("Payment added successfully");
+
+     }
+
+
+
      // method to display all the transactions
      public List<Transaction> getAllTransactions (){
           return transactions;
      }
+
+
 }
 
 
--- ledger-accounting/src/main/java/com/pluralsight/ledgerAccounting/Main.java
+++ ledger-accounting/src/main/java/com/pluralsight/ledgerAccounting/Main.java
@@ -2,11 +2,15 @@ package com.pluralsight.ledgerAccounting;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
+    static Scanner mainScanner = new Scanner(System.in);
     public static void main(String[] args) throws IOException {
         Ledger ledger = new Ledger();
+        homeScreen();
 
+        /*
         try {
             ledger.loadTransactions("./src/main/resources/transactions.csv");
             List<Transaction> allTransactions = ledger.getAllTransactions();
@@ -15,5 +19,44 @@ public class Main {
             throw new RuntimeException(e);
         }
 
+         */
+
+    }
+
+
+    private static void homeScreen () {
+        boolean counter = true; //Loop control variable
+        while (counter) {
+            System.out.println("""
+                    ===========================================
+                       Welcome to the accounting application
+                    ===========================================
+                          Please select an option
+                           D- Add Deposit
+                           P- Make a Payment
+                           L- Ledger
+                           R- Reports
+                           X- Exit
+                    ==========================================
+                    """);
+            String option = mainScanner.nextLine().trim();
+
+            switch (option.toUpperCase()) {
+                case "D":
+                    break;
+                case "P":
+                    break;
+                case "L":
+                    break;
+                case "R":
+                    break;
+                case "X":
+                    System.out.println("Exiting the Accounting Application");
+                    counter = false;
+                    break;
+                default:
+                    System.out.println("Invalid option. Please type 'D' 'P' 'L' 'R' or 'X'");
+            }
+        }
     }
 }
