0f1cb394f86dad60c36c46ef70a16a7a3d08b92c
Deibcr <deibcr@gmail.com>
2024-10-17 16:23:01 -0400

    Class Ledger is mark complete because was copy from the last version of the github repo, was accidentally delete while trying to get familiar with uiDesigner in IntelliJ, those GUIforms are not going to be committed yet, meanwhile Main and Ledger have implementations of ResourceBundle and property files to read  (2) .properties with .txt options ES and EN of every prompt, also connected with 2 different .cvs to save the data en the user prefer language (This also show the lack of adaptability of this way to implement a multilingual app.

--- ledger-accounting/src/main/java/com/pluralsight/ledgerAccounting/Main.java
+++ ledger-accounting/src/main/java/com/pluralsight/ledgerAccounting/Main.java
@@ -1,23 +1,59 @@
 package com.pluralsight.ledgerAccounting;
 
+
+
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.ResourceBundle;
+import java.util.Locale;
+
 
 public class Main {
     static Scanner mainScanner = new Scanner(System.in);
-    static Ledger ledger = new Ledger();
+    static Ledger ledger;
+    static ResourceBundle messages;
+    static String transactionsPath;
 
 
     public static void main(String[] args) throws IOException {
+        selectLanguage();
+        ledger = new Ledger(messages,transactionsPath);
 
         try {
-            ledger.loadTransactions("./src/main/resources/transactions.csv");
+            ledger.loadTransactions(transactionsPath);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
+
         ledger.homeScreen(mainScanner);
 
+
+
+    }
+
+    private static void selectLanguage(){
+        System.out.println(" Select your preferred language / Seleccione su idioma preferido:");
+        System.out.println("1. English");
+        System.out.println("2. Spanish");
+
+        int chooseLanguage = mainScanner.nextInt();
+        mainScanner.nextLine();
+
+        switch (chooseLanguage){
+            case 2:
+                setLanguage("es", "ES"); //Spanish
+                transactionsPath= "./src/main/resources/transacciones.csv";
+                break;
+            default:
+                setLanguage("en", "US"); //Default to English
+                transactionsPath= "./src/main/resources/transactions.csv";
+        }
+    }
+
+    private static void setLanguage(String lang, String country){
+        Locale locale = new Locale (lang, country);
+        messages = ResourceBundle.getBundle("messages", locale);
     }
 
 }
